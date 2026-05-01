c952730d56cea813312234129efa6f0145a5df9a
Alexandre St-fort <solonoobhacker6767+@gmail.com>
2026-05-01 00:04:52 -0400

    Update networking_resources.tf

--- gcp-landing-zone/modules/networking_resources.tf
+++ gcp-landing-zone/modules/networking_resources.tf
@@ -4,12 +4,32 @@ resource "google_compute_network" "shared_vpc" {
   project                 = google_project.shared_net.project_id
 }
 
+# --- Development Subnet ---
+resource "google_compute_subnetwork" "dev_subnet" {
+  name          = "dev-subnet-01"
+  ip_cidr_range = "10.0.2.0/24"
+  region        = var.region
+  network       = google_compute_network.shared_vpc.id
+  project       = google_project.shared_net.project_id
+  private_ip_google_access = true
+}
+
+# --- Testing Subnet ---
+resource "google_compute_subnetwork" "test_subnet" {
+  name          = "test-subnet-01"
+  ip_cidr_range = "10.0.3.0/24"
+  region        = var.region
+  network       = google_compute_network.shared_vpc.id
+  project       = google_project.shared_net.project_id
+  private_ip_google_access = true
+}
+
+# --- Production Subnet (Already have this) ---
 resource "google_compute_subnetwork" "prod_subnet" {
   name          = "prod-subnet-01"
   ip_cidr_range = "10.0.1.0/24"
-  region        = "northamerica-northeast1"
+  region        = var.region
   network       = google_compute_network.shared_vpc.id
   project       = google_project.shared_net.project_id
-
   private_ip_google_access = true
 }
